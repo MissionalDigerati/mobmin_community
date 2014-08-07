b8ebf2eab4cc84cbae165742e1d033f1a03417fa
Codemis <johnathan@missionaldigerati.com>
2014-08-06 17:12:03 -0700

    Capitalizing directories.

--- webroot2/libs/dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("EZSQL_DB_USER", 'root');
-define("EZSQL_DB_PASSWORD", 'root');
-define("EZSQL_DB_NAME", 'pligg_mobmin');
-define("EZSQL_DB_HOST", 'localhost');
-if (!function_exists('gettext')) {
-	function _($s) {return $s;}
-}
-?>
--- webroot2/settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-$my_base_url = 'http://mobmin.pligg.local';
-$my_pligg_base = '';
-$dblang = 'en';
-define('table_prefix', '');
-$language = 'english';
-include_once mnminclude.'settings_from_db.php';
-?>
