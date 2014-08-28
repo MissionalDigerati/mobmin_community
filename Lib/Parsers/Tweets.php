7b5371496530a970971a9541c8780931b470bb8d
Codemis <johnathan@missionaldigerati.com>
2014-08-28 15:20:49 -0700

    Setup a Tweet Parser class, and the first steps towards defining the parser.

---
+++ Lib/Parsers/Tweets.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * This file is part of #MobMin Community.
+ * 
+ * #MobMin Community is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Joshua Project API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see 
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @author Johnathan Pulos <johnathan@missionaldigerati.org>
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * 
+ */
+namespace Parsers;
+
+/**
+ * The Link Resource for managing links to the Pligg site
+ */
+class Tweets
+{
+    /**
+     * The Embedly Object for retrieving the link information
+     *
+     * @var \Embedly\Embedly
+     * @access protected
+     **/
+    protected $embedly;
+    /**
+     * The Slugify Object for creating a slug from a string
+     *
+     * @var \Cocur\Slugify\Slugify
+     * @access protected
+     **/
+    protected $slugify;
+    /**
+     * The links that have been parsed from the Twittter response
+     *
+     * @var array
+     * @access protected
+     **/
+    protected $tweetedLinks = array();
+    /**
+     * Construct the class
+     *
+     * @param \Embedly\Embedly $embedlyObj The Embedly object for retrieving link information
+     * @param \Cocur\Slugify\Slugify $slugifyObj The Slugify object for turning strings into slugs
+     * @return void
+     * @access public
+     * @author Johnathan Pulos
+     **/
+    public function __construct($embedlyObj, $slugifyObj)
+    {
+        $this->setEmbedlyObject($embedlyObj);
+        $this->setSlugifyObject($slugifyObj);
+    }
+    /**
+     * Sets the $embedly class variable
+     *
+     * @param \Embedly\Embedly $embedlyObj The Embedly object for retrieving link information
+     * @return void
+     * @access protected
+     * @throws InvalidArgumentException if $embedlyObj is not a \Embedly\Embedly Object
+     * @author Johnathan Pulos
+     **/
+    protected function setEmbedlyObject($embedlyObj)
+    {
+        if (is_a($embedlyObj, '\Embedly\Embedly')) {
+            $this->embedly = $embedlyObj;
+        } else {
+            throw new \InvalidArgumentException('$embedlyObj must be of the class \Embedly\Embedly.');
+        }
+    }
+    /**
+     * Sets the $slugify class variable
+     *
+     * @param \Cocur\Slugify\Slugify $slugifyObj The Slugify object for turning strings into slugs
+     * @return void
+     * @access protected
+     * @throws InvalidArgumentException if $slugifyObj is not a \Cocur\Slugify\Slugify Object
+     * @author Johnathan Pulos
+     **/
+    protected function setSlugifyObject($slugifyObj)
+    {
+        if (is_a($slugifyObj, '\Cocur\Slugify\Slugify')) {
+            $this->slugify = $slugifyObj;
+        } else {
+            throw new \InvalidArgumentException('$slugifyObj must be of the class \Cocur\Slugify\Slugify.');
+        }
+    }
+    /**
+     * Parses the data that is retrieved from the API.  The data should be a JSON object.
+     *
+     * @param object $response The response object from the API
+     * @return array The links to save
+     * @access public
+     * @author Johnathan Pulos
+     **/
+    public function parseLinksFromAPI($response)
+    {
+        foreach ($response->statuses as $tweet) {
+            $links = $tweet->entities->urls;
+            foreach ($links as $link) {
+                $linkData = array(
+                    "link_url"  =>  $link->expanded_url
+                );
+                array_push($this->tweetedLinks, $linkData);
+            }
+        }
+        return $this->tweetedLinks;
+    }
+}
---
+++ Tests/Unit/Lib/Parsers/TweetsTest.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * This file is part of #MobMin Community.
+ * 
+ * #MobMin Community is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Joshua Project API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see 
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @author Johnathan Pulos <johnathan@missionaldigerati.org>
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * 
+ */
+namespace Tests\Unit\Lib\Parsers;
+
+/**
+ * Test the Link Resource
+ *
+ * @author Johnathan Pulos
+ */
+class TweetsTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * A JSON Object that represents the response of Twitter's API
+     *
+     * @var Object
+     * @access private
+     **/
+    private $searchTweetsFactory;
+    /**
+     * Setup the testing
+     *
+     * @return void
+     * @access public
+     * @author Johnathan Pulos
+     **/
+    public function setUp()
+    {
+        $DS = DIRECTORY_SEPARATOR;
+        $jsonFile = __DIR__ . $DS . ".." . $DS . ".." . $DS . ".." . $DS . "Support" . $DS . "Factories" . $DS . "SearchTweets.json";
+        $this->searchTweetsFactory = json_decode(file_get_contents($jsonFile));
+    }
+    /**
+     * __construct should throw an error if passed a non \Embedly\Embedly object for embedly
+     *
+     * @return void
+     * @access public
+     * @expectedException InvalidArgumentException
+     * @author Johnathan Pulos
+     **/
+    public function testConstructThrowsErrorIfGivenANonEmbedlyObject()
+    {
+        $this->setupTweetsParser('Fake Embedly Object', null);
+    }
+    /**
+     * __construct should throw an error if passed a non \Cocur\Slugify\Slugify object for slugify
+     *
+     * @return void
+     * @access public
+     * @expectedException InvalidArgumentException
+     * @author Johnathan Pulos
+     **/
+    public function testConstructThrowsErrorIfGivenANonSlugifyObject()
+    {
+        $this->setupTweetsParser(null, 'Fake Slugify Object');
+    }
+    /**
+     * parseLinksFromAPI() should return an array for each link in the JSON object
+     *
+     * @return void
+     * @access public
+     * @author Johnathan Pulos
+     **/
+    public function testParseLinksFromAPIShouldReturnAllLinksSeperated()
+    {
+        $expectedLinks = array(
+            "http://weadapt.org/knowledge-base/improving-access-to-climate-adaptation-information/mwash",
+            "http://lyricspro.net/",
+            "http://yahoo.com/"
+        );
+        $expectedLength = count($expectedLinks);
+        $tweetsParser = $this->setupTweetsParser(null, null);
+        $linkData = $tweetsParser->parseLinksFromAPI($this->searchTweetsFactory);
+        $links = array();
+        $this->assertFalse(empty($linkData));
+        foreach ($linkData as $link) {
+            array_push($links, $link['link_url']);
+        }
+        $this->assertEquals($expectedLength, count($links));
+        foreach ($expectedLinks as $expectedLink) {
+            $this->assertTrue(in_array($expectedLink, $links));
+        }
+    }
+    /**
+     * Sets up a Tweets object with the given objects
+     *     
+     * @param \Embedly\Embedly $embedlyObj The Embedly object for retrieving link information
+     * @param \Cocur\Slugify\Slugify $slugifyObj The Slugify object for turning strings into slugs
+     * @return \Parsers\Tweets
+     * @access protected
+     * @author Johnathan Pulos
+     **/
+    protected function setupTweetsParser($embedlyObj = null, $slugifyObj = null)
+    {
+        if (is_null($embedlyObj)) {
+            $embedlyObj = $this->getMock('\Embedly\Embedly', array('oembed'), array());
+        }
+        if (is_null($slugifyObj)) {
+            $slugifyObj = $this->getMock('\Cocur\Slugify\Slugify', array('slugify'));
+        }
+        return new \Parsers\Tweets($embedlyObj, $slugifyObj);
+    }
+}
