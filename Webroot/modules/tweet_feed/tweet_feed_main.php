ab926dd9463c026e8405f28e63160fa5a79d72bd
Codemis <johnathan@jpulos.com>
2016-02-04 15:39:18 -0800

    Changed the direction the Twitter feed is going.

--- Webroot/modules/tweet_feed/tweet_feed_main.php
+++ Webroot/modules/tweet_feed/tweet_feed_main.php
@@ -1,24 +1,24 @@
 <?php
 /**
  * This file is part of #MobMin Community.
- * 
+ *
  * #MobMin Community is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Joshua Project API is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see 
+ * along with this program.  If not, see
  * <http://www.gnu.org/licenses/>.
  *
  * @author Johnathan Pulos <johnathan@missionaldigerati.org>
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
- * 
+ *
  */
 function tweet_feed_get_current_page($totalPages) {
     $currentPage = (isset($_REQUEST['current-page'])) ? intval($_REQUEST['current-page']) : 1;
@@ -46,7 +46,7 @@ function tweet_feed_get_tweets()
      */
     $query = "SELECT tf.tweet_feed_id, tf.tweet_id, tf.tweeter_id, tf.tweeter_name, tf.content, tf.published_date, " .
         "ta.tweeter_avatar_url FROM " . table_prefix . "tweet_feed as tf JOIN " . table_prefix . "tweet_feed_avatars as ta ON " .
-        "tf.tweeter_id = ta.tweeter_id ORDER BY tf.published_date ASC LIMIT " . $offset . ", " . $resultsPerPage;
+        "tf.tweeter_id = ta.tweeter_id ORDER BY tf.published_date DESC LIMIT " . $offset . ", " . $resultsPerPage;
     $results = $db->get_results($query);
     $tweets = array();
     foreach ($results as $result) {
